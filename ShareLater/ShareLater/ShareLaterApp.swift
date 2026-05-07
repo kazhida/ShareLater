a587df316e43b5a45776803df55ffa765453dea3
kazhida <kazhida@gmail.com>
2026-05-07 19:22:49 +0900

    アプリ作成の初期状態

---
+++ ShareLater/ShareLater/ShareLaterApp.swift
@@ -0,0 +1,32 @@
+//
+//  ShareLaterApp.swift
+//  ShareLater
+//
+//  Created by 樋田一幸 on 2026/05/07.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ShareLaterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
